d84ce0b31fa018df9a1bf303f035785c9b9626f1
Nelson Valero Barcelona <169047839+Nelvb@users.noreply.github.com>
2024-10-29 21:23:11 +0000

    empezando about

--- src/js/layout.js
+++ src/js/layout.js
@@ -3,29 +3,28 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/home";
-import { AboutMe } from "./views/aboutMe";
+import { AboutMe } from "./views/aboutMe";  // Importamos el componente AboutMe
 import injectContext from "./store/appContext";
 
 import { Footer } from "./component/footer";
 
 const Layout = () => {
-	// Define el basename solo si tu proyecto se publica en un subdirectorio
-	const basename = process.env.BASENAME || "";
+    const basename = process.env.BASENAME || "";
 
-	return (
-		<div>
-			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/about" element={<AboutMe />} /> {/* Nueva ruta AboutMe */}
-						<Route path="*" element={<h1>Not found!</h1>} />
-					</Routes>
-					<Footer />
-				</ScrollToTop>
-			</BrowserRouter>
-		</div>
-	);
+    return (
+        <div>
+            <BrowserRouter basename={basename}>
+                <ScrollToTop>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<AboutMe />} /> {/* Activamos la ruta AboutMe */}
+                        <Route path="*" element={<h1>Not found!</h1>} />
+                    </Routes>
+                    <Footer />
+                </ScrollToTop>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default injectContext(Layout);
--- src/js/views/aboutMe.js
+++ src/js/views/aboutMe.js
@@ -2,15 +2,14 @@ import React from "react";
 import "../../styles/aboutMe.css";
 
 export const AboutMe = () => (
-  <div className="container-fluid home-container d-flex align-items-center justify-content-center">
-    <div className="row inner-frame justify-content-center">
+  <div className="container-fluid about-container d-flex align-items-center justify-content-center">
+    <div className="row about-inner-frame justify-content-center">
       {/* Contenido de la sección About Me */}
-      <div className="col-12 text-center">
-        <h1 className="title-text">Sobre Mí</h1>
+      <div className="col-12 text-left">
+        <h1 className="about-title-text">Sobre Mí</h1>
       </div>
       <div className="col-10 mt-4">
         <p className="about-text">
-          {/* Texto de ejemplo sobre el autor */}
           Soy un desarrollador Full Stack apasionado por construir aplicaciones
           web funcionales y atractivas. A lo largo de mi formación, he
           trabajado con tecnologías como React, Flask, y bases de datos
