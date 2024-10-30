0a7284962b83d7987d446524fc9459fde0964060
Nelson Valero Barcelona <169047839+Nelvb@users.noreply.github.com>
2024-10-30 00:04:01 +0000

    animacion en el home

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -6,32 +6,51 @@ import logoNel from "../../img/logosinfondo_5.png";
 
 export const Home = () => {
   useEffect(() => {
+    // Animación del logo
     anime({
-      targets: ".title-text", // Selecciona el texto por la clase
-      opacity: [0, 1],         // Aparece de manera suave
-      translateY: [-20, 0],    // Anima de arriba hacia abajo
+      targets: ".logo-image",
+      opacity: [0, 1],
+      duration: 2000,
+      easing: "easeInOutQuad",
+      delay: 500, // Retardo antes de que aparezca el logo
+    });
+
+    // Animación del título
+    anime({
+      targets: ".title-text",
+      opacity: [0, 1],
+      translateY: [-20, 0],
       easing: "easeInOutQuad",
       duration: 2000,
-      delay: 500,
+      delay: 3000, // Espera a que el logo termine de aparecer
+    });
+
+    // Animación de los enlaces
+    anime({
+      targets: ".navigation-links li",
+      opacity: [0, 1],
+      translateX: [-50, 0],
+      easing: "easeInOutQuad",
+      duration: 1000,
+      delay: anime.stagger(200, { start: 4000 }), // Aparece uno a uno después del título
+      complete: () => {
+        // Añade la clase para el efecto de brillo al borde después de que aparezca el último enlace
+        document.querySelector(".inner-frame").classList.add("glow-effect");
+      }
     });
   }, []);
 
   return (
     <div className="container-fluid home-container d-flex align-items-center justify-content-center">
       <div className="row inner-frame justify-content-center">
-        {/* Columna para el título */}
         <div className="col-12 text-center">
           <div className="title-container">
             <h1 className="title-text">Full Stack Developer</h1>
           </div>
         </div>
-
-        {/* Columna para el logo */}
         <div className="col-12 d-flex justify-content-center logo-container">
           <img src={logoNel} alt="Logo Nelson Valero" className="logo-image" />
         </div>
-
-        {/* Columna para los enlaces de navegación */}
         <nav className="col-3 d-flex flex-column align-items-center navigation-links">
           <ul className="list-unstyled">
             <li><Link to="/about">Sobre mí</Link></li>
