c0c9abfbeb52ea47a7c9e1ef507dfa614442cfd5
Nelvb <nelsonvbarcelona@gmail.com>
2025-03-12 10:02:01 +0100

    Updated webpack and vercel configuration

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,14 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const Dotenv = require('dotenv-webpack');
+const path = require('path');
+
 module.exports = merge(common, {
     mode: 'production',
     output: {
-        publicPath: '/Nelvb-portfolio/',
+        path: path.resolve(__dirname, 'build'),
+        publicPath: '/',
+        filename: 'bundle.js'
     },
     plugins: [
         new Dotenv({
